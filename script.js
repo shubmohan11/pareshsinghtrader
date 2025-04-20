adb452bde4ee8f02b8e218dc00a4273720f569c8
Shubham Mohan <30762257+shubmohan11@users.noreply.github.com>
2025-04-20 20:20:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const app = document.getElementById("app");
+
+app.innerHTML = `
+  <img
+    src="https://yt3.googleusercontent.com/98aup-R6uKe0Po_H-_IbBgxrr3SWUFfNNtAQcegGW8aUryi6OfGv3Q0mQ5mbxfkYdCZEH6KC=w2560-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj"
+    alt="Paresh Singh"
+    class="profile"
+  />
+  <h1>Hi, I'm Paresh Singh</h1>
+  <a
+    href="https://t.me/pareshsinghsupport"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="telegram-link"
+  >
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/1024px-Telegram_logo.svg.png"
+      alt="Telegram"
+      class="telegram-icon"
+    />
+    <br />
+    Click To Join
+  </a>
+  <p class="description">
+    Welcome to Paresh Singh Family, your ultimate destination for mastering
+    the art of trading! Whether you're a seasoned trader looking to
+    fine-tune your strategies or a beginner eager to dive into the world of
+    financial markets, this channel is your go-to resource. Join me, Paresh,
+    as I break down complex trading concepts into easy-to-understand
+    lessons, providing you with practical tips and techniques to navigate
+    the markets with confidence. From technical analysis to risk management,
+    I cover it all, ensuring you have the knowledge and skills to succeed in
+    your trading journey. With a focus on forex, crypto, commodities,
+    stocks, options, and more, Paresh Singh Family is your one-stop shop for
+    all things trading. Don't miss out on the latest market updates, trading
+    tutorials, and insightful discussions that will take your trading game
+    to the next level. Subscribe now and embark on your path to trading
+    success with Paresh Singh Family!
+  </p>
+`;
